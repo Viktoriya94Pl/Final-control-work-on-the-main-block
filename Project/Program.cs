4e7a8baa255098222dafd1dc30ad390d29009780
Vika Platonova <vplatonova_1994@mail.ru>
2023-06-07 20:44:35 +0300

    Make CreateNewArray function

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,3 +6,16 @@
     }
     Console.WriteLine();
 }
+void CreateNewArray(string[] array, string [] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if(array[i].Length <=3)
+        {
+          array2[count] = array[i];
+          count++;
+        }
+    }
+    
+}
