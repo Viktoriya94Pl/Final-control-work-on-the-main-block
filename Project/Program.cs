f656960a05e4187bbd3564e74fb915c76a7cdd38
Vika Platonova <vplatonova_1994@mail.ru>
2023-06-08 21:50:23 +0300

    rev2

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,12 +6,13 @@
     }
     Console.WriteLine();
 }
-void CreateNewArray(string[] array, string [] array2)
+void CreateNewArray(string[] array, string[] array2)
 {
+    int maxSynmol = 3;
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     { 
-        if(array[i].Length <=3)
+        if(array[i].Length <= maxSynmol)
         {
           array2[count] = array[i];
           count++;
@@ -20,10 +21,24 @@ void CreateNewArray(string[] array, string [] array2)
     
 }
 
+int FindSumElements (string[] array)
+{
+    int count = 0;
+    int maxSynmol = 3;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if(array[i].Length <= maxSynmol)
+        {
+          count++;
+        }
+    }return count;
+}
+
 void FinalTask()
 {
-string [] array = {"Hello","2","world",")","-4","cat","dog","print"};
-string[] array2 = new string[array.Length];
+string [] array = {"Hello","2","world","dr","-4","cat","dog","print","ty"};
+int size = FindSumElements(array);
+string [] array2 = new string [size];
 CreateNewArray(array, array2);
 PrintArray(array2);
 }
